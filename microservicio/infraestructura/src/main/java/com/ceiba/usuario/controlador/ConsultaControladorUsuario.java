ba9368dbbbb87bf34223cfa5498ba178a8124f99
randon93 <32597818+randon93@users.noreply.github.com>
2020-12-30 11:37:55 -0500

    ajustes sonar-project.properties

--- microservicio/infraestructura/src/main/java/com/ceiba/usuario/controlador/ConsultaControladorUsuario.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/usuario/controlador/ConsultaControladorUsuario.java
@@ -35,7 +35,7 @@ public class ConsultaControladorUsuario {
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("Bucar Usuarios")
+    @ApiOperation("Bucar Usuario")
     public DtoUsuario buscar(@PathVariable Long id) {
         return this.manejadorBuscarUsuario.ejecutar(id);
     }
